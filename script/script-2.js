37df1c08f598fe5bbd4e4ee336eed0e74b2e65ee
rachid MBoutou <SARAH GAX@Rachid.titan.lan>
2025-06-10 22:26:07 +0100

    first commit

---
+++ script/script-2.js
@@ -0,0 +1,39 @@
+const buttons =  document.querySelectorAll(".cat-texte");
+const catImage = document.querySelectorAll(".groy-categorie")
+
+const filterCat = (e)=> {
+    document.querySelector(".active").classList.remove('active');
+    e.target.classList.add("active")
+
+
+    catImage.forEach(groyCategorie => {
+        groyCategorie.classList.add("filtreure");
+
+        if(groyCategorie.dataset.name=== e.target.dataset.name
+            || e.target.dataset.name === "Tous"){
+                 groyCategorie.classList.remove("filtreure");
+            }
+    })
+}
+
+buttons.forEach(catTexte => catTexte.addEventListener("click", filterCat));
+//---------------------------------------------------------
+
+const Menuhumberger = document.querySelector(".menu-navigation");
+const menu = document.querySelector(".nav-links");
+const bodyStyle =document.querySelector("body");
+
+
+Menuhumberger.addEventListener("click", () =>{
+    Menuhumberger.classList.toggle("active");
+    menu.classList.toggle("active");
+    bodyStyle.classList.toggle("active");
+
+})
+
+
+
+
+
+
+
---
+++ script/script.js
@@ -0,0 +1,16 @@
+//----------------------------------------------
+gsap.to(".nav-logo",{ 
+    x: 10,
+    duration: 2,
+    opacity: 1,
+})
+
+gsap.to(".img-rotation",{ 
+    duration: 4 ,
+    repeat:-1,
+    delay: 4,
+    ease: "bounce",
+    rotation: 360,
+})
+
+//------------------------------------------------------
