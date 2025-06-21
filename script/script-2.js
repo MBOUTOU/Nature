6f9ff58721c6b363c088bffc8cd2cecc4c8fdfaf
rachid MBoutou <SARAH GAX@Rachid.titan.lan>
2025-06-22 00:11:14 +0100

    first commit

--- script/script-2.js
+++ script/script-2.js
@@ -5,15 +5,14 @@ const filterCat = (e)=> {
     document.querySelector(".active").classList.remove('active');
     e.target.classList.add("active")
 
-
-    catImage.forEach(groyCategorie => {
+    catImage.forEach((groyCategorie) => {
         groyCategorie.classList.add("filtreure");
 
         if(groyCategorie.dataset.name=== e.target.dataset.name
             || e.target.dataset.name === "Tous"){
                  groyCategorie.classList.remove("filtreure");
             }
-    })
+    })  
 }
 
 buttons.forEach(catTexte => catTexte.addEventListener("click", filterCat));
@@ -21,13 +20,15 @@ buttons.forEach(catTexte => catTexte.addEventListener("click", filterCat));
 
 const Menuhumberger = document.querySelector(".menu-navigation");
 const menu = document.querySelector(".nav-links");
-const bodyStyle =document.querySelector("body");
+const bodyStyle = document.querySelector("body");
+const parentLinks = document.querySelector(".nav-link-contanaire")
 
 
 Menuhumberger.addEventListener("click", () =>{
     Menuhumberger.classList.toggle("active");
     menu.classList.toggle("active");
     bodyStyle.classList.toggle("active");
+    parentLinks.classList.toggle("active")
 
 })
 
