b87e8896b7d57c50cbadb46b8ce4741e51148e11
rachid MBoutou <SARAH GAX@Rachid.titan.lan>
2025-09-16 14:54:53 +0100

    repos X modification prod

---
+++ script/produit.js
@@ -0,0 +1,47 @@
+const boutons = document.querySelectorAll('.Donne');
+const cartes = document.querySelectorAll('.card');
+
+boutons.forEach((button) =>{
+   button.addEventListener("click", () =>{
+      const Name = button.getAttribute("data-name");
+
+    //   les element actives
+      boutons.forEach((b) =>{b.classList.remove("active");})
+      button.classList.add("active");
+
+      cartes.forEach((carte) =>{
+        if(carte.getAttribute("data-name") === Name){
+            carte.classList.add("active");
+        }else{
+            carte.classList.remove("active")
+        }
+      })
+   })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
