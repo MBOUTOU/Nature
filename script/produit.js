e4dea0a5abc7377ea1ce887231c12be167644ddd
rachid MBoutou <SARAH GAX@Rachid.titan.lan>
2025-09-19 12:05:33 +0100

    repos X modification prod 4

--- script/produit.js
+++ script/produit.js
@@ -22,11 +22,58 @@ boutons.forEach((button) =>{
 // ---------------------------------------------------------
  
 document.addEventListener("DOMContentLoaded", () =>{
-  const PageUrl = window.location.href;
+  const URldelapage = encodeURIComponent(window.location.href);
+  const MesaageProduit = document.getElementById("message-produit");
+  const DataMessage = encodeURIComponent(MesaageProduit.dataset.message);
+  
+  
+  function partagerSurFacebook() {
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${URldelapage }`, '_blank', 'noopener,noreferrer');
+  };
 
-    if(PageUrl.includes("localhost")){
-      PageUrl = "";
-    }
+  function partagerSurWhatsApp() {
+    window.open(`https://api.whatsapp.com/send?text=${DataMessage}%20${URldelapage }`, '_blank', 'noopener,noreferrer');
+  };
+
+  function ouvrirInstagram() {
+    window.open(`https://www.instagram.com/ton_nom_d_utilisateur/`, '_blank', 'noopener,noreferrer');
+  };
+
+//  function partagerParGmail() {
+   
+// }
+
+  function ouvrirTikTok() {
+    window.open(`https://www.tiktok.com/@ton_nom_d_utilisateur`, '_blank');
+  }
+
+
+const BtnFacebook = document.querySelector(".bnt-Facebook");
+const BtnWhatsApp = document.querySelector(".btn-Whatsapp");
+const BtnMail = document.querySelector(".btn-Mail");
+const BtnTiktok = document.querySelector(".btn-tiktok");
+const BtnInstagram = document.querySelector(".btn-instagram")
+
+if(BtnFacebook){
+  BtnFacebook.addEventListener('click',partagerSurFacebook())
+}
+
+if(BtnInstagram){
+  BtnInstagram.addEventListener('click', ouvrirInstagram())
+}
+
+if(BtnWhatsApp){
+  BtnWhatsApp.addEventListener('click', partagerSurWhatsApp() )
+}
+
+if(BtnTiktok){
+   BtnTiktok.addEventListener('click', ouvrirTikTok())
+}
+
+// if(BtnMail){
+//   BtnMail.addEventListener('click', partagerParGmail())
+// }
+
 })
 
 
