d1043aa0b72c159055ef83fe2cf068498642c443
rachid MBoutou <SARAH GAX@Rachid.titan.lan>
2025-09-20 21:34:21 +0100

    repos X modification prod 2

--- script/produit.js
+++ script/produit.js
@@ -71,6 +71,57 @@ if(BtnTiktok){
 
 })
 
+// -------------------------------------------------------
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 30,
+  autoplay :{
+    delay: 5000,
+    pauseOnMouseEnter: true,
+  },
+
+  // If we need pagination
+  pagination: {
+    el:'.swiper-pagination',
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 40,
+    },
+    750: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  },
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
